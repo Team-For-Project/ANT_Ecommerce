b55e308edff130b1b6ba220cb8dbaeaf555ad813
tes1 <tes1>
2020-01-05 18:40:17 +0100

    modification1

--- ANT_Ecommerce/Program.cs
+++ ANT_Ecommerce/Program.cs
@@ -16,7 +16,7 @@ namespace ANT_Ecommerce
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Form());
         }
     }
 }
